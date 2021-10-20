f1bade88d3ee522ad55b96ee802a8c766d3923a0
jesstphan <92838597+jesstphan@users.noreply.github.com>
2021-10-20 01:00:46 -0400

    Update LED_ControlBackground.py

--- LED_ControlBackground.py
+++ LED_ControlBackground.py
@@ -5,16 +5,26 @@
 
 import RPi.GPIO as GPIO
 import time
+import json
 
-ledPin = 19 #Green
-ledPin = 20 #Blue 
-ledPin = 21 #White 
+ledPin1 = 19 #Green
+ledPin2 = 20 #Blue 
+ledPin3 = 21 #White 
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(ledPin, GPIO.OUT)
+GPIO.setup(ledPin1, GPIO.OUT)
+GPIO.setup(ledPin2, GPIO.OUT)
+GPIO.setup(ledPin3, GPIO.OUT)
 
-pwm = GPIO.PWM(ledPin, 100) # PWM object on our pin at 100 Hz
-pwm.start(0) # start with LED off
+with open('led-pwm.txt', 'r') as f:
+data = json.load(f)
+
+pwm1 = GPIO.PWM(ledPin1, 100) # PWM object on our pin at 100 Hz
+pwm2 = GPIO.PWM(ledPin2, 100)
+pwm3 = GPIO.PWM(ledPin3, 100)
+pwm1.start(0) # start with LED off
+pwm2.start(0)
+pwm3.start(0)
 
 while True:
   with open("led-pwm.txt", 'r') as f:
