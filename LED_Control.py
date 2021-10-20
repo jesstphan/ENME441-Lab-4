15a0ec4739cae7f1f4bf8a6bbf1ba3fbd36077c9
jesstphan <92838597+jesstphan@users.noreply.github.com>
2021-10-20 00:36:37 -0400

    Update LED_Control.py

--- LED_Control.py
+++ LED_Control.py
@@ -17,8 +17,16 @@ GPIO.setup(ledPin3, GPIO.OUT)
 data = cgi.FieldStorage()
 s1 = data.getvalue('slider1')
 
+if ('LED Green' in data): 
+  GPIO.output(ledPin1, 1)
+elif ( 'LED Blue' in data):
+  GPIO.output(ledPin2, 1)
+elif ( 'LED White' in data):
+  GPIO.output(ledPin3, 1)
+
 with open('led-pwm.txt', 'w') as f:  
   f.write(str(s1))
+
 
 print('Content-type: text/html\n\n')
 print('<html>')
@@ -31,3 +39,4 @@ print('<input type="submit" value="Change LED brightness">')
 print('</form>')
 print(‘Brightness = %s' % s1)
 print('</html>')
+
